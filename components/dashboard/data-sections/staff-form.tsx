d71b3cf0f407a0ed35d995a887a494975ba05e93
aminofabian <aminofab@gmail.com>
2025-12-20 10:54:51 +0300

    changed password requirement

--- components/dashboard/data-sections/staff-form.tsx
+++ components/dashboard/data-sections/staff-form.tsx
@@ -146,7 +146,7 @@ export const StaffForm = ({ staff, onSubmit, onCancel, isLoading }: StaffFormPro
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
               <p className="text-xs text-amber-800 dark:text-amber-300">
-                <span className="font-medium">Password Requirements:</span> Must be at least 5 characters long. Choose a strong password to secure the manager&apos;s account.
+                <span className="font-medium">Password Requirements:</span> Must be at least 8 characters long. Choose a strong password to secure the manager&apos;s account.
               </p>
             </div>
           </div>
